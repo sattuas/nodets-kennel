7ac42f79c9eb68bf16b311f8b50826087c9fb953
Raphael Melo <rafael.dks10@gmail.com>
2023-01-28 12:23:15 -0300

    creating the routes and controllers

---
+++ src/controllers/pageController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+
+export const home = (req: Request, res: Response) => {
+    //res.render()
+}
+
+export const dogs = (req: Request, res: Response) => {
+    //res.render()
+}
+
+export const cats = (req: Request, res: Response) => {
+    //res.render()
+}
+
+export const fishes = (req: Request, res: Response) => {
+    //res.render()
+}

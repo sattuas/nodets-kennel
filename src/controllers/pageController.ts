902e67bfc496469eb23622eba64836f31d607c3c
Raphael Melo <rafael.dks10@gmail.com>
2023-01-30 14:23:29 -0300

    separating the views

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 
 export const home = (req: Request, res: Response) => {
-    //res.render()
+    res.render('pages/page')
 }
 
 export const dogs = (req: Request, res: Response) => {
